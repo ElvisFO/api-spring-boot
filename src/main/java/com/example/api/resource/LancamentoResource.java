90b5e9fb84d305cfea6ca648492c0655c4d1859a
Elvis Fernandes <elvisfernandesc@gmail.com>
2017-12-03 18:05:32 -0200

    Salvando lançamento e buscando por codigo

--- src/main/java/com/example/api/resource/LancamentoResource.java
+++ src/main/java/com/example/api/resource/LancamentoResource.java
@@ -1,11 +1,19 @@
 package com.example.api.resource;
 
+import java.net.URI;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.api.model.Lancamento;
 import com.example.api.repository.LancamentoRepository;
@@ -21,5 +29,21 @@ public class LancamentoResource {
 	public List<Lancamento> findAll()
 	{
 		return lancamentoRepository.findAll();
+	}
+	
+	@PostMapping
+	public ResponseEntity<?> save(@RequestBody Lancamento lancamento, HttpServletResponse response)
+	{
+		Lancamento lancamentoSalvo = lancamentoRepository.save(lancamento);
+		URI uri =  ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+				.buildAndExpand(lancamentoSalvo.getCodigo()).toUri();
+		response.setHeader("Location", uri.toASCIIString());
+		
+		return ResponseEntity.created(uri).body(lancamentoSalvo);
+	}
+	
+	@GetMapping("/{codigo}")
+	public Lancamento findOne(@PathVariable Long codigo) {
+		return lancamentoRepository.findOne(codigo);
 	}
 }
