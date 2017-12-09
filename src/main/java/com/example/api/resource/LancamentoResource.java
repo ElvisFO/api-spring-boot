88fbf516e447f37c0108621b062a65df2e0117e2
Elvis Fernandes <elvisfernandesc@gmail.com>
2017-12-09 18:04:00 -0200

    Deletando lancamento, ajustando busca por codigo e tratando exceção EmptyResultDataAccessException

--- src/main/java/com/example/api/resource/LancamentoResource.java
+++ src/main/java/com/example/api/resource/LancamentoResource.java
@@ -9,11 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.api.event.RecursoCriadoEvent;
@@ -45,7 +47,33 @@ public class LancamentoResource {
 	}
 	
 	@GetMapping("/{codigo}")
-	public Lancamento findOne(@PathVariable Long codigo) {
-		return lancamentoRepository.findOne(codigo);
+	public ResponseEntity<?>  findOne(@PathVariable Long codigo) {
+		Lancamento lancamento = lancamentoRepository.findOne(codigo);
+		
+		return lancamento != null ? ResponseEntity.ok(lancamento) : ResponseEntity.notFound().build();
 	}
+	
+	@DeleteMapping("/{codigo}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void delete(@PathVariable Long codigo)
+	{
+		lancamentoRepository.delete(codigo);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
